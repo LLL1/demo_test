038b40781ef2fd247684423aa15981d8f2cf4444
lizhijie <809322316@qq.com>
2018-09-10 13:40:17 +0800

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	@GetMapping("/")
+	String home() {
+		return "Spring is 123456!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	public void deletes(){
+
+	}
+}
